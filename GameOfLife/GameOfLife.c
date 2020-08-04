a42471c373c1a76054975f07b999d6f16733b4ca
Starkey <neron.seb@gmail.com>
2020-08-04 11:09:21 -0400

    allocated bigger stack reserve 4MB

--- GameOfLife/GameOfLife.c
+++ GameOfLife/GameOfLife.c
@@ -10,43 +10,62 @@
 *   Copyright (c) 2016 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
-
 #include "raylib.h"
+#include <string.h>
 
-#define MAX_BUILDINGS   100
+#define MAX_GRID_X 400
+#define MAX_GRID_Y 400
+
+#define SCREENWIDTH 800
+#define SCREENHEIGHT 800
+
+#define CELL_SIZE 2
+#define CELL_POW CELL_SIZE / 2
+
+#define for_x for (int x = 0; x < MAX_GRID_X; x++)
+#define for_y for (int y = 0; y < MAX_GRID_Y; y++)
+#define for_xy for_x for_y
+
+#define NUM_THREADS 4
+
+
+void evolve(unsigned cells[MAX_GRID_X][MAX_GRID_Y])
+{
+    //divide in threads
+    unsigned newcells[MAX_GRID_X][MAX_GRID_Y];
+    int n = 0;
+
+    for_y for_x{
+            n = 0;
+            for (unsigned int y1 = y - 1; y1 <= y + 1; y1++)
+                for (unsigned int x1 = x - 1; x1 <= x + 1; x1++)
+                    if (!cells[(x1 + MAX_GRID_X) % MAX_GRID_X][(y1 + MAX_GRID_Y) % MAX_GRID_Y])
+                        n++;
+            if (!cells[x][y]) n--;
+            newcells[x][y] = (n == 3 || (n == 2 && !cells[x][y])) > 0 ? 0 : 255;
+    }
+
+    //after threads
+    for_y for_x cells[x][y] = newcells[x][y];
+
+}
 
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, "Game of Life");
 
-    InitWindow(screenWidth, screenHeight, "Game of Life");
+    Rectangle player = { SCREENWIDTH / 2, SCREENHEIGHT / 2, 0, 0 };
+    unsigned cells[MAX_GRID_X][MAX_GRID_Y];
 
-    Rectangle player = { 400, 280, 40, 40 };
-    Rectangle buildings[MAX_BUILDINGS] = { 0 };
-    Color buildColors[MAX_BUILDINGS] = { 0 };
-
-    int spacing = 0;
-
-    for (int i = 0; i < MAX_BUILDINGS; i++)
-    {
-        buildings[i].width = GetRandomValue(50, 200);
-        buildings[i].height = GetRandomValue(100, 800);
-        buildings[i].y = screenHeight - 130 - buildings[i].height;
-        buildings[i].x = -6000 + spacing;
-
-        spacing += buildings[i].width;
-
-        buildColors[i] = (Color){ GetRandomValue(200, 240), GetRandomValue(200, 240), GetRandomValue(200, 250), 255 };
-    }
+    for_xy cells[x][y] = rand()%100 < 50 ? 0 : 255;
 
     Camera2D camera = { 0 };
-    camera.target = (Vector2){ player.x + 20, player.y + 20 };
-    camera.offset = (Vector2){ screenWidth / 2, screenHeight / 2 };
+    camera.target = (Vector2){ player.x + CELL_SIZE / 2, player.y + CELL_SIZE / 2 };
+    camera.offset = (Vector2){ SCREENWIDTH / 2, SCREENHEIGHT / 2 };
     camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+    camera.zoom = 0.9f;
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -58,15 +77,18 @@ int main(void)
         //----------------------------------------------------------------------------------
 
         // Player movement
-        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
-        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
+        if (IsKeyDown(KEY_D)) player.x += 20;
+        else if (IsKeyDown(KEY_A)) player.x -= 20;
+
+        if (IsKeyDown(KEY_S)) player.y += 20;
+        else if (IsKeyDown(KEY_W)) player.y -= 20;
 
         // Camera target follows player
-        camera.target = (Vector2){ player.x + 20, player.y + 20 };
+        camera.target = (Vector2){ player.x + 5, player.y + 5 };
 
         // Camera rotation controls
-        if (IsKeyDown(KEY_A)) camera.rotation--;
-        else if (IsKeyDown(KEY_S)) camera.rotation++;
+        if (IsKeyDown(KEY_Q)) camera.rotation--;
+        else if (IsKeyDown(KEY_E)) camera.rotation++;
 
         // Limit camera rotation to 80 degrees (-40 to 40)
         if (camera.rotation > 40) camera.rotation = 40;
@@ -84,6 +106,9 @@ int main(void)
             camera.zoom = 1.0f;
             camera.rotation = 0.0f;
         }
+
+        evolve(cells);
+
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -94,36 +119,15 @@ int main(void)
 
         BeginMode2D(camera);
 
-        DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+        for_xy DrawRectangleRec((Rectangle) { x<<CELL_POW, y<<CELL_POW, CELL_SIZE, CELL_SIZE }, (Color) {cells[x][y],cells[x][y],cells[x][y],255});
 
-        for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
-
-        DrawRectangleRec(player, RED);
-
-        DrawLine(camera.target.x, -screenHeight * 10, camera.target.x, screenHeight * 10, GREEN);
-        DrawLine(-screenWidth * 10, camera.target.y, screenWidth * 10, camera.target.y, GREEN);
 
         EndMode2D();
-
-        DrawText("SCREEN AREA", 640, 10, 20, RED);
-
-        DrawRectangle(0, 0, screenWidth, 5, RED);
-        DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-        DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-        DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
-
-        DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(10, 10, 250, 113, BLUE);
-
-        DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
-        DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
-        DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-        DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
-        DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
+
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
